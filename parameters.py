ed36f4c1f06ebdc02662d37bca231026171f824f
weibac <milan.weibel@uc.cl>
2023-01-05 18:35:38 -0600

    parametrization

--- parameters.py
+++ parameters.py
@@ -1,8 +1,8 @@
-import os
+import os.path as p
 
 
 NAME_CUBE9 = 'Boby Centered Cubic of the RGB cube'
 NAME_RAINBOW = 'BGW and the rainbow'
 
-PATH_CUBE9 = os.path.join('palettes', 'cube9.csv')
-PATH_RAINBOW = os.path.join('palettes', 'rainbow_bgw.csv')
+PATH_CUBE9 = p.join('palettes', 'cube9.csv')
+PATH_RAINBOW = p.join('palettes', 'rainbow_bgw.csv')
