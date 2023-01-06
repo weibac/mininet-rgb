06ad1c51f72c5f127aa8d9d7b13915f00189e629
weibac <milan.weibel@uc.cl>
2023-01-05 18:54:39 -0600

    removed unused coloring library from repo

--- parameters.py
+++
@@ -1,8 +0,0 @@
-import os.path as p
-
-
-NAME_CUBE9 = 'Boby Centered Cubic of the RGB cube'
-NAME_RAINBOW = 'BGW and the rainbow'
-
-PATH_CUBE9 = p.join('palettes', 'cube9.csv')
-PATH_RAINBOW = p.join('palettes', 'rainbow_bgw.csv')
