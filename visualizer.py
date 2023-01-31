05d6eb7e00e7addbe6d74f5f8497420d808a8b87
weibac <milan.weibel@uc.cl>
2023-01-31 17:15:51 -0300

    refactor and feat: display multiple models at once

--- visualizer.py
+++ visualizer.py
@@ -5,28 +5,30 @@ from os.path import join
 from colorlibrary.colors import Colorer
 from trainer import Net
 
-
-path = join("model_weights", "4417acc0-62.json")
-with open(path, 'r') as param_file:
-    params = json.load(param_file)
-
-fc1_weights = params["fc1_weights"]
-fc1_biases = params["fc1_biases"]
-fc2_weights = params["fc2_weights"]
-fc2_biases = params["fc2_biases"]
-
-fc1_weights = torch.nn.parameter.Parameter(torch.tensor(fc1_weights))
-fc1_biases = torch.nn.parameter.Parameter(torch.tensor(fc1_biases))
-fc2_weights = torch.nn.parameter.Parameter(torch.tensor(fc2_weights))
-fc2_biases = torch.nn.parameter.Parameter(torch.tensor(fc2_biases))
-
-
 colorer = Colorer('', '')
-net = Net(input_size=3, hidden_size=12, output_size=9)
-net.fc1.weight = fc1_weights
-net.fc1.bias = fc1_biases
-net.fc2.weight = fc2_weights
-net.fc2.bias = fc2_biases
+
+
+def load_net(filename):
+    path = join("model_weights", filename)
+    with open(path, 'r') as param_file:
+        params = json.load(param_file)
+
+    fc1_weights = params["fc1_weights"]
+    fc1_biases = params["fc1_biases"]
+    fc2_weights = params["fc2_weights"]
+    fc2_biases = params["fc2_biases"]
+    fc1_weights = torch.nn.parameter.Parameter(torch.tensor(fc1_weights))
+    fc1_biases = torch.nn.parameter.Parameter(torch.tensor(fc1_biases))
+    fc2_weights = torch.nn.parameter.Parameter(torch.tensor(fc2_weights))
+    fc2_biases = torch.nn.parameter.Parameter(torch.tensor(fc2_biases))
+
+    net = Net(input_size=3, hidden_size=12, output_size=9)
+    net.fc1.weight = fc1_weights
+    net.fc1.bias = fc1_biases
+    net.fc2.weight = fc2_weights
+    net.fc2.bias = fc2_biases
+
+    return net, fc1_weights
 
 
 def to_rgb_scale(value: float):
@@ -44,14 +46,22 @@ def display_color_acts(color_acts, labels):
     print()
 
 
-print("Hidden layer activation visualization")
-fc1_color_acts = to_rgb_scale(fc1_weights)
-display_color_acts(fc1_color_acts, False)
+def visualize(net, fc1_weights):
+    print("Hidden layer activation visualization")
+    fc1_color_acts = to_rgb_scale(fc1_weights)
+    display_color_acts(fc1_color_acts, False)
 
-print("Output layer activation visualization")
-categories = ['black', 'grey/gray', 'white', 'red', 'orange', 'yellow',
-              'green', 'blue', 'violet']
-out_acts = net(torch.eye(3, 3))  # Identity matrix
-out_acts = out_acts.transpose(0, 1)
-out_color_acts = to_rgb_scale(out_acts)
-display_color_acts(out_color_acts, categories)
+    print("Output layer activation visualization")
+    categories = ['black', 'grey/gray', 'white', 'red', 'orange', 'yellow',
+                  'green', 'blue', 'violet']
+    out_acts = net(torch.eye(3, 3))  # Identity matrix
+    out_acts = out_acts.transpose(0, 1)
+    out_color_acts = to_rgb_scale(out_acts)
+    display_color_acts(out_color_acts, categories)
+
+
+nets = ["4417acc0-62.json", "9440acc0-62.json", "79acc0-6.json"]
+
+for net in nets:
+    print(f"\n\nNeural net in file {net}\n")
+    visualize(*load_net(net))
